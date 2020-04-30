2a8c1ca33918021ebbec295234115ae7db10592d
Chinwer <qiuyh0924@163.com>
2020-04-30 16:01:38 +0800

    feat: finish part of mine page

--- app/src/main/java/com/tsinghua/tsinghelper/ui/mine/MineFragment.java
+++ app/src/main/java/com/tsinghua/tsinghelper/ui/mine/MineFragment.java
@@ -1,6 +1,23 @@
 package com.tsinghua.tsinghelper.ui.mine;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.tsinghua.tsinghelper.R;
+
 public class MineFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_mine, container, false);
+
+        return root;
+    }
 }
