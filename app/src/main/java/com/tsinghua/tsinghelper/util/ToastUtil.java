f88e061510b5d0a4187abecac10bff5f2f27b7eb
QiuYuanhui <qiuyh0924@163.com>
2020-05-06 22:59:29 +0800

    chore: add showToastOnUIThread to ToastUtil

--- app/src/main/java/com/tsinghua/tsinghelper/util/ToastUtil.java
+++ app/src/main/java/com/tsinghua/tsinghelper/util/ToastUtil.java
@@ -1,12 +1,15 @@
 package com.tsinghua.tsinghelper.util;
 
+import android.app.Activity;
 import android.content.Context;
 import android.widget.Toast;
-
-import androidx.annotation.Nullable;
 
 public class ToastUtil {
     public static void showToast(Context context, String text) {
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void showToastOnUIThread(Activity activity, String text) {
+        activity.runOnUiThread(() -> showToast(activity, text));
     }
 }
