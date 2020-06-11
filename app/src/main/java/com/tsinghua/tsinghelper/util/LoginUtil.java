21e590d4db999314ba03f190b070bf990cf5126e
Chinwer <qiuyh0924@163.com>
2020-06-11 12:50:44 +0800

    ui: add mask effect to profile bg

--- app/src/main/java/com/tsinghua/tsinghelper/util/LoginUtil.java
+++ app/src/main/java/com/tsinghua/tsinghelper/util/LoginUtil.java
@@ -8,16 +8,13 @@ public class LoginUtil {
     public static boolean isLoggedIn(Context context) {
         SharedPreferences sharedPreferences = UserInfoUtil.getUserInfoSharedPreferences();
         String username = sharedPreferences.getString("username", "");
-        if (username.isEmpty()) {
-            // not logged in yet
-            return false;
-        }
+        // not logged in yet
+        return !username.isEmpty();
 
         // TODO: check validity of the user id
-        return true;
     }
 
-    public static void clearUserInfo(Context context) {
+    public static void clearUserInfo() {
         SharedPreferences sharedPreferences = UserInfoUtil.getUserInfoSharedPreferences();
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.clear();
